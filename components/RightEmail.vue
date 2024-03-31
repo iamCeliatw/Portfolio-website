b76bca7e1fc7720baa8229159b0641d31871a691
Celia Chang <tina8899530@gmail.com>
2024-03-31 13:58:23 +0800

    layout css style and navbar hover style

---
+++ components/RightEmail.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.rightemail__wrapper 
+  .rightemail__container
+    p tina8899530@gmail.com
+    hr
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="sass" scoped>
+.rightemail__wrapper
+  position: fixed
+  right: 45px
+  bottom: 45px
+  p
+    writing-mode: vertical-rl
+    color: #8284BA
+.rightemail__container
+  font-weight: 600
+  font-size: 20px
+  display: flex
+  flex-direction: column
+  align-items: center
+  gap: 5px
+hr
+  height: 140px
+  border-width: 2px
+  border-style: solid
+  border-color: rgb(209 213 219 / var(--tw-border-opacity))
+  --tw-border-opacity: 1
+</style>
---
+++ layouts/custom.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template lang="pug">
+div.custom__wrapper
+  Header
+  LeftLink
+  RightEmail
+  slot
+</template>
+
+<style lang="sass" scoped>
+.custom__wrapper
+  position: relative
+</style>
